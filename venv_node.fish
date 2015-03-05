2689765556850f3dd14c9145de3a6562078e0b55
dzikie drożdże <daz@hackerspace.pl>
2015-03-05 10:24:41 +0100

    derp

--- venv_node.fish
+++ venv_node.fish
@@ -1,21 +1,17 @@
 function deactivate_node
 
-    if test -n "$_OLD_NODE_VIRTUAL_PATH"
-        set -xg NPM_CONFIG_PREFIX $_OLD_NPM_CONFIG_PREFIX
-        set -xg npm_config_prefix $_OLD_npm_config_prefix
+    set -xg NPM_CONFIG_PREFIX $_OLD_NPM_CONFIG_PREFIX
+    set -xg npm_config_prefix $_OLD_npm_config_prefix
 
-        set -e $_OLD_npm_config_prefix
-        set -e $_OLD_NPM_CONFIG_PREFIX
-    end
+    set -e $_OLD_npm_config_prefix
+    set -e $_OLD_NPM_CONFIG_PREFIX
 
     if test "$argv[1]" != "nondestructive"
         functions -e deactivate_node
     end
 end
 
-set -l NODE_VIRTUAL_ENV (dirname (status -f))
-
 set _OLD_NPM_CONFIG_PREFIX "$NPM_CONFIG_PREFIX"
 set _OLD_npm_config_prefix "$npm_config_prefix"
-set -x NPM_CONFIG_PREFIX "$NODE_VIRTUAL_ENV"
-set -x npm_config_prefix "$NODE_VIRTUAL_ENV"
+set -x NPM_CONFIG_PREFIX "$VIRTUAL_ENV"
+set -x npm_config_prefix "$VIRTUAL_ENV"
